35fe1a598fbc5c3771138d98552e3005f3b4f556
William Wilhelm <WillNathaniel.Wilhelm@cba.com.au>
2018-12-11 13:03:57 +1100

    Revert "Try adding a script for cron to call"
    This reverts commit 8fcab146f5455d32d27f22f4c41f835fbb0f9a3e.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "$(dc -e "$(date +"%M") 2 + 60 % p") * * * * /tmp/nolog_shepherd.sh" | crontab -
+echo "$(dc -e "$(date +"%M") 2 + 60 % p") * * * * sh -c '/home/shepherd/.shepherd/applications/shepherd/shepherd --nolog > /proc/self/fd/1 2>/proc/self/fd/2'" | crontab -
 
 exec cron -f
 
