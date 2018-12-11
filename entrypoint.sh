68a760c45ae72fdf6e4f5eede03a0198904ddca1
William Wilhelm <WillNathaniel.Wilhelm@cba.com.au>
2018-12-11 11:32:31 +1100

    Fix cron syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "$(dc -e "$(date +"%M") 2 + 60 % p") * * * * * /home/shepherd/.shepherd/applications/shepherd/shepherd" | crontab -
+echo "$(dc -e "$(date +"%M") 2 + 60 % p") * * * * /home/shepherd/.shepherd/applications/shepherd/shepherd" | crontab -
 
 cron
 
